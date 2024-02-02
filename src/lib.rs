a9b6d44783a25d647b4bfb47b89aea758d04e016
pwbh <127856937+pwbh@users.noreply.github.com>
2024-02-02 13:04:27 +0200

    Add specific test for set fucntion

--- src/lib.rs
+++ src/lib.rs
@@ -230,6 +230,25 @@ mod tests {
 
     #[async_std::test]
     #[cfg_attr(miri, ignore)]
+    async fn set_returns_ok() {
+        let mut storage = Storage::new(&function!(), false).await.unwrap();
+
+        let value = r#"
+        {"id":8,"title":"Microsoft Surface Laptop 4","description":"Style and speed. Stand out on ...","price":1499,"discountPercentage":10.23,"rating":4.43,"stock":68,"brand":"Microsoft Surface","category":"laptops","thumbnail":"https://cdn.dummyjson.com/product-images/8/thumbnail.jpg","images":["https://cdn.dummyjson.com/product-images/8/1.jpg","https://cdn.dummyjson.com/product-images/8/2.jpg","https://cdn.dummyjson.com/product-images/8/3.jpg","https://cdn.dummyjson.com/product-images/8/4.jpg","https://cdn.dummyjson.com/product-images/8/thumbnail.jpg"]}
+        "#;
+
+        storage.set(value.as_bytes()).await.unwrap();
+
+        // Make sure all messages are written to the disk before we continue with our tests
+        storage.flush().await.unwrap();
+
+        let result = storage.get(0).await.unwrap();
+
+        assert_eq!(result, value.as_bytes());
+    }
+
+    #[async_std::test]
+    #[cfg_attr(miri, ignore)]
     async fn get_returns_ok() {
         let message_count = 500;
         let test_message = b"messssagee";
